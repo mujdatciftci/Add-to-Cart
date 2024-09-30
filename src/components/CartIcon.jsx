bb5a32ef0c4edbaf2e84893875d8b2ddb6bcea4c
Müjdat Çiftçi <mujdatciftci88@gmail.com>
2024-09-30 23:11:11 +0300

    fixed func

--- src/components/CartIcon.jsx
+++ src/components/CartIcon.jsx
@@ -1,12 +1,18 @@
 import { IoMdCart } from "react-icons/io";
-const CartIcon = () => {
+import PropTypes from 'prop-types';
+const CartIcon = ({cart}) => {
+
   return (
        <div className="relative">
               <IoMdCart className="text-3xl" />
               <span className="bg-red-600 text-sm w-4 h-4 flex 
-              justify-center items-center rounded-full absolute -top-1 -right-1 text-white ">0</span>
+              justify-center items-center rounded-full absolute -top-1 -right-1 text-white ">{cart.length}</span>
        </div>
   )
-}
+};
 
+CartIcon.propTypes = {
+  cart: PropTypes.array.isRequired,
+  setCart: PropTypes.func.isRequired,
+};
 export default CartIcon
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,17 @@
 import CartIcon from "./CartIcon"
+import PropTypes from 'prop-types';
 
-const Header = () => {
+const Header = ({cart}) => {
   return (
     <div className="flex justify-between items-center">
        <h1 className="text-4xl py-5">Add to Cart</h1>
-       <CartIcon />
+       <CartIcon cart={cart} />
     </div>
   )
 }
 
+Header.propTypes = {
+  cart: PropTypes.array.isRequired,
+  setCart: PropTypes.func.isRequired,
+};
 export default Header
